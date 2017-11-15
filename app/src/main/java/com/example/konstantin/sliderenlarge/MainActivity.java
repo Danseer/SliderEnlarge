9cdebc5b0c5f5f6e60f4ceb62bd3cbadba2a0093
Konstantin <mr.ks83@mail.ru>
2017-11-15 16:35:01 +0200

    fff

--- app/src/main/java/com/example/konstantin/sliderenlarge/MainActivity.java
+++ app/src/main/java/com/example/konstantin/sliderenlarge/MainActivity.java
@@ -1,28 +1,45 @@
 package com.example.konstantin.sliderenlarge;
 
-import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.facebook.drawee.backends.pipeline.Fresco;
 import com.stfalcon.frescoimageviewer.ImageViewer;
 
+
 public class MainActivity extends AppCompatActivity {
     private String[] list;
     private static final String POSTERS_PATH = "https://raw.githubusercontent.com/stfalcon-studio/FrescoImageViewer/v.0.5.0/images/posters";
+    private ImageOverlayView overlayView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Fresco.initialize(this);
-        list=getPosters();
 
-        new ImageViewer.Builder(this, list)
-                .setStartPosition(1)
-                .show();
+        Fresco.initialize(this);
+        list = getPosters();
+
+        showPict();
+
+
+
     }
+
+
+private void showPict (){
+    overlayView = new ImageOverlayView(this);
+    new ImageViewer.Builder(this, list)
+            .setStartPosition(1)
+            .setOverlayView(overlayView)
+            .setImageChangeListener(getImageChangeListener())
+
+            .show();
+}
 
     public static String[] getPosters() {
         return new String[]{
@@ -35,6 +52,17 @@ public class MainActivity extends AppCompatActivity {
                 POSTERS_PATH + "/Frank.jpg",
                 POSTERS_PATH + "/Max.jpg",
                 POSTERS_PATH + "/Daniel.jpg"
+        };
+    }
+
+    private ImageViewer.OnImageChangeListener getImageChangeListener() {
+        return new ImageViewer.OnImageChangeListener() {
+            @Override
+            public void onImageChange(int position) {
+               // CustomImage image = images.get(position);
+               overlayView.setShareText("text share "+list[position]);
+               overlayView.setDescription("text description "+ position);
+            }
         };
     }
 }
